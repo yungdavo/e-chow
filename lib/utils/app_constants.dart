8da86f486530f181230c44f86b02ac916e3fbe67
yungdavo <113898608+yungdavo@users.noreply.github.com>
2025-06-26 08:11:28 +0100

    initial commit

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants{
   static const String APP_NAME= "e-CHOW";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL= "http://127.0.0.1:8000"; //"https://mvs.bslmeiyu.com"
+  static const String BASE_URL= "http://10.0.2.2:8000"; //"https://mvs.bslmeiyu.com"
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular" ;
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended" ;
 
